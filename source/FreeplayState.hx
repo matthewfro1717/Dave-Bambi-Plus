204791965fa195976524ad518c10cc9f4a79e508
matthewfro1717 <114807610+matthewfro1717@users.noreply.github.com>
2023-05-08 18:30:30 -0400

    Update FreeplayState.hx

--- source/FreeplayState.hx
+++ source/FreeplayState.hx
@@ -172,7 +172,7 @@ class FreeplayState extends MusicBeatState
 	var characterSelectText:FlxText;
 	var showCharText:Bool = true;
 
-	var checker:FlxBackdrop;
+	var checker:FlxBackdrop = new FlxBackdrop(Paths.image('ui/checkeredBG'), 0.2, 0.2, true, true);
 	var gradientBar:FlxSprite = new FlxSprite(0, 0).makeGraphic(FlxG.width, 300, 0xFFfd719b);
 
 	override function create()
@@ -214,12 +214,9 @@ class FreeplayState extends MusicBeatState
 			gradientBar.scrollFactor.set(0, 0);
 			gradientBar.antialiasing = FlxG.save.data.globalAntialiasing;
 
-			#if (flixel < "5.3.0")
-			checker:FlxBackdrop = new FlxBackdrop(Paths.image('ui/checkeredBG'), 0.2, 0.2, true, true);
+			add(checker);
 			checker.scrollFactor.set(0, 0.07);
 			checker.antialiasing = FlxG.save.data.globalAntialiasing;
-			add(checker);
-			#end
 		}
 		if (FlxG.save.data.terminalFound && !awaitingExploitation)
 		{
