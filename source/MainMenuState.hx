d5803a0802afdc70a0e382fdbcf157e135b49454
matthewfro1717 <114807610+matthewfro1717@users.noreply.github.com>
2023-05-08 17:12:41 -0400

    Update MainMenuState.hx

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -211,7 +211,7 @@ class MainMenuState extends MusicBeatState
 			gradientBar.scrollFactor.set(0, 0);
 			add(gradientBar);
 			gradientBar.antialiasing = FlxG.save.data.globalAntialiasing;
-                 
+
 			#if (flixel < "5.3.0")
                         checker:FlxBackdrop = new FlxBackdrop(Paths.image('ui/checkeredBG'), 0.2, 0.2, true, true);
 			checker.scrollFactor.set(0, 0.07);
